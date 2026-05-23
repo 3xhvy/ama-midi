4834350366940fcee6cf7440973c5b395ff47e4c
hohoanghvy <huyhohoang1907@gmail.com>
2026-05-24 02:48:25 +0700

    (fix)deployment: prisma

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "$DATABASE_URL" ]; then
 fi
 
 echo "Running database migrations..."
-./node_modules/.bin/prisma migrate deploy
+prisma migrate deploy
 
 echo "Starting API..."
 exec node dist/main
